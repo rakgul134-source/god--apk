e55e8710029aa4fff55ddb793aeda4136e3a078e
rakgul134-source <rakgul134@gmail.com>
2026-04-17 23:33:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,4 @@ class PredatorApp(App):
     def build(self):
         return Label(text='IBLIS AYAKTA AVCI 👑\nAPK CALISIYOR')
 
-PredatorApp().run()[app]
-title = Predator
-package.name = predator
-package.domain = org.iblis
-
-source.include_exts = py
-version = 0.1
-
-requirements = python3,kivy
-
-[buildozer]
-log_level = 2
-
-android.api = 33
-android.minapi = 21
-android.ndk = 25b
-android.sdk_path =
-android.ndk_path =
-android.archs = arm64-v8a, armeabi-v7abuildozer android debug
+PredatorApp().run()
