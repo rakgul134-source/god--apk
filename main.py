0789621b45886e464847d5f36cefc472523aa9da
rakgul134-source <rakgul134@gmail.com>
2026-04-17 19:50:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 
-class GodApp(App):
-    def build(self):
-        return Label(text='TANRI APK Çalışıyor 👑')
-
-GodApp().run()
+class TanriApp(App):
+    def tiklandi(self, instance):
+        popup = Popup(title='TANRI APK',
+                      content=Label(text='ÇALIŞIYOR KRAL!'),
+                      size_hint=(0.6, 0.4))
+        popup.open()
