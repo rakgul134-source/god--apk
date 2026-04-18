f82c8ef5665f6f95e5c8c69d794170bb571f9829
rakgul134-source <rakgul134@gmail.com>
2026-04-18 10:21:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-class PredatorApp(App):
+class AvciAvApp(App):
     def build(self):
-        return Label(text='IBLIS AYAKTA AVCI 👑\nAPK CALISIYOR')
-
-PredatorApp().run()
+        self.layout = BoxLayout(orientation='vertical', padding=40, spacing=20)
+        
+        self.oran_input = TextInput(
+            hint_text='Oran gir: 4.31',
+            multiline=False,
+            input_filter='float',
+            font
