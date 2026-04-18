cf9236050da9c298c65e30dc8db6c312fcd132c1
rakgul134-source <rakgul134@gmail.com>
2026-04-18 16:55:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
 
 Window.clearcolor = (0.1, 0.1, 0.1, 1)
 
@@ -12,4 +13,11 @@ class AvciAvApp(App):
         self.layout = BoxLayout(orientation='vertical', padding=20, spacing=15)
         
         self.baslik = Label(
-            text='AVCI 
+            text='AVCI AV 01 1 0 KURALI',
+            font_size='24sp',
+            size_hint_y=0.2,
+            color=get_color_from_hex('#00FF00')
+        )
+        
+        self.input = TextInput(
+            hint_text='Ondalikli sayi gir: 2.05',
